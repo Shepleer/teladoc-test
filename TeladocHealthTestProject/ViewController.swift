5fc3da274f7199d5b8c4f40fc03d571c175534d9
Ivan Shepler <IDShpilevskiy@sberbank.ru>
2021-08-14 00:16:48 +0300

    Initial Commit

---
+++ TeladocHealthTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeladocHealthTestProject
+//
+//  Created by Ivan Shepler on 14/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
